be89faa75e42b9861127f9ed4e459b22bf7147c3
Jason <bennguyen20122006@gmail.com>
2025-10-31 22:18:30 -0700

    changed files

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,11 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
-	/* config options here */
-	reactCompiler: true,
-	turbopack: {
-		root: path.join(__dirname, "..")
-	}
+  /* config options here */
+  reactCompiler: true,
+  turbopack: {
+    root: path.join(__dirname, ".."),
+  },
 };
 
 export default nextConfig;
